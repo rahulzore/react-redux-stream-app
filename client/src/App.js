dbc2a12cbf3a363dc939ca299d7d2908101f4348
Rahul Zore <zore.r@husky.neu.edu>
2019-05-04 12:17:12 -0400

    created components and wired the routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './components/streams/StreamCreate';
+import StreamDelete from './components/streams/StreamDelete';
+import StreamEdit from './components/streams/StreamEdit';
+import StreamList from './components/streams/StreamList';
+import StreamShow from './components/streams/StreamShow';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
